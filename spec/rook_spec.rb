9e66682b08e6209ee7ca974db2f4e6fb43e56c6c
Brett Tanner <bretttanner171@gmail.com>
2022-09-06 21:35:24 +0900

    Generate Rook Possible moves

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -4,4 +4,18 @@ require './lib/rook.rb'
 
 describe Rook do
   subject(:rook) {described_class.new}
+
+  describe "#legal?" do
+    context "When the move is legal" do
+      it "returns true" do
+        
+      end
+    end
+
+    context "When the move isn't legal" do
+      it "returns false" do
+        
+      end
+    end
+  end
 end
