defd5c8987f31bfaefc9994115c7aec195c1cf39
Brett Tanner <bretttanner171@gmail.com>
2022-09-07 19:50:21 +0900

    Test Rook#legal?

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -3,18 +3,51 @@
 require './lib/rook.rb'
 
 describe Rook do
-  subject(:rook) {described_class.new}
+  subject(:rook) {described_class.new("white")}
+
+  before do
+    allow(rook).to receive(:puts)
+  end
 
   describe "#legal?" do
+    let(:board) {Array.new}
+
     context "When the move is legal" do
-      it "returns true" do
-        
+      it "moves unlimited spaces horizontally" do
+        start = [1, 1]
+        dest = [1, 8]
+        legal = rook.legal?(start, dest, board)
+        expect(legal).to be true
+      end
+
+      it "moves unlimited spaces vertically" do
+        start = [1, 1]
+        dest = [8, 1]
+        legal = rook.legal?(start, dest, board)
+        expect(legal).to be true
       end
     end
 
     context "When the move isn't legal" do
-      it "returns false" do
-        
+      it "doesn't move in an L-shape" do
+        start = [4, 4]
+        dest = [6, 3]
+        legal = rook.legal?(start, dest, board)
+        expect(legal).to be false
+      end
+
+      it "doesn't move to a random space" do
+        start = [4, 4]
+        dest = [7, 8]
+        legal = rook.legal?(start, dest, board)
+        expect(legal).to be false
+      end
+
+      it "doesn't move diagonally" do
+        start = [4, 4]
+        dest = [5, 5]
+        legal = rook.legal?(start, dest, board)
+        expect(legal).to be false
       end
     end
   end
